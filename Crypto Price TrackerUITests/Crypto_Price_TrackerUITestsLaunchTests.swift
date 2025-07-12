c8a2b0ae34fa41f611805445bf491c5e94143e26
Abhishek Kapoor <40399665+theabhieye@users.noreply.github.com>
2025-07-12 18:37:42 +0530

    Initial Commit

---
+++ Crypto Price TrackerUITests/Crypto_Price_TrackerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Crypto_Price_TrackerUITestsLaunchTests.swift
+//  Crypto Price TrackerUITests
+//
+//  Created by Abhishek kapoor on 12/07/25.
+//
+
+import XCTest
+
+final class Crypto_Price_TrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
